b4382f364deed9de2dd5fc999fdcf15be0fbc79e
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-04-10 00:22:40 -0700

    Added 4 layers of choices

--- script.js
+++ script.js
@@ -71,9 +71,79 @@ const gameConfig = [
   },
   {
     choice: "Try to pet it",
-    prompt: "1b The dog licks your hand, and allows you to pet it. You read his collar and his name is Spike. You call him by his name and he wags his tail excitedly. You walk around and Spike follows you.",
-    options: ["Go home", "Take Spike for a walk"]
-  }
+    prompt: "1b The dog licks your hand, and allows you to pet it. You read his collar and his name is Spike. You call him by his name and he wags his tail excitedly. You walk around a lake and Spike follows you. You stop for a moment while Spike sniffs around, but wanders off.",
+    options: ["Follow Spike", "Call Spike to come back"]
+  },
+  {
+    choice: "Follow Spike",
+    prompt: "2a He leads you to a little box on the ground. You hold him back, afraid of what the object might be. Upon further investigation, you discover it's a tiny house! You look closer and see a tiny flicker of light on the inside. Spike tries to push past you to get to the house. Do you let him get to house?",
+    options: ["Let him","Don't let him"]
+  },
+  {
+    choice: "Let him",
+    prompt: `3a Spike runs towards the house and sniffs aggressively, shaking the tiny house. You see the door open and a tiny fairy screams! It waves it's hand, releasing a puff of glittery dust into Spike's face. The door closes and Spike is dazed, but after a few moments, remains unharmed. Do you interogate the fairy or avoid conflict?`,
+    options: ["Interogate the fairy","Avoid conflict"]
+  },
+  {
+    choice: "Interogate the fairy",
+    prompt: "4a The fairy opens the door, apologizing. It was startled and accidentally charmed Spike. The fairy, named Fawn, gives Spike an antidote to reverse the charm. Fawn is sweet and caring to animals. You become friends and you offer to take Fawn home to live with you. Fawn accepts. You, Spike, and Fawn go home to live together. END",
+    options: ["Restart"]
+  },
+  {
+    choice: "Avoid conflict",
+    prompt: `4b During the walk home, Spike starts to cough. You grow concerned as the coughing lasts a few minutes. You ask, "Are you okay?" You hear a voice respond, "Yes, I'm fine." Spike spoke! The fairy seems to have charmed Spike into being able to speak. You take him home and have interesting conversations. END`,
+    options: ["Restart"]
+  },
+  {
+    choice: "Don't let him",
+    prompt: "3b You continue to hold him back. The tiny door opens, revealing a little fairy! She cautiously flies over to you. Her name is Fawn. She tells you about her fairy friends and a leprechaun who also lives at the lake.",
+    options: ["Visit her friends","Visit the leprechaun"]
+  },
+  {
+    choice: "Visit her friends",
+    prompt: "4a Fawn flies away and comes back with her friends. They make both of you flower crowns and gently place them on your heads. They fly around you, giggling and playing with Spike. You and Spike leave for the day, but always make time to see your new fairy friends.END ",
+    options: ["Restart"]
+  },
+  {
+    choice: "Visit the leprechaun",
+    prompt: "4b Fawn takes you down to the leprechaun's home. She introduces you, and he welcomes you with open arms. You are amazed with his interior design skills, and he offers to decorate your house. You take him up on his offer and he leaves you with a beautiful cottagecore home. END",
+    options: ["Restart"]
+  },
+  {
+    choice: "Call Spike to come back",
+    prompt: "2b Spike comes running back to you. You continue walking and find a small home. You knock on the door, and a leprechaun opens the door! He invites you inside. His name is Albie, and he just finished baking an apple pie. He shares a slice with you, while he gives you a tour of his home. You see a room with a golden statues and gold coins.",
+    options: ["Ask about the gold statues","Ask about the gold coins"]
+  },
+  {
+    choice: "Ask about the gold statues",
+    prompt: "3a Albie shows you his collection of awards he won during the Leprechaun Olympics. He is a professional pickle ball player. He offers to be your pickle ball mentor. Do you accept?",
+    options: ["Learn pickle ball","Refuse"]
+  },
+  {
+    choice: "Learn pickle ball",
+    prompt: "4a You and Albie practice pickle ball for months to compete in a pickle ball tournament. You win numerous awards, thanks to your mentor. END",
+    options: ["Restart"]
+  },
+  {
+    choice: "Refuse",
+    prompt: `4b "No worries, it's not for everyone" he says. He tells you stories of his competitions and he gives you the recipe for his amazing apple pie. END`,
+    options: ["Restart"]
+  },
+  {
+    choice: "Ask about the gold coins",
+    prompt: "3b Albie tells you that after every rainfall, the leprechauns have a gold hunt. They race to the end of the rainbow and the winner gets the gold. He asks if you would like to join during the next hunt, but warns that the other leprachauns are competitive and violent.",
+    options: ["Join the gold hunt","Pass"]
+  },
+  {
+    choice: "Join the gold hunt",
+    prompt: "4a You wait for the next rainy day and join Albie in the gold hunt. Due to your human size, you carry Ablie and move faster than the other teams. You beat everyone to the end of the rainbow and win the gold prize! Albie gives you the entire pot of gold, because he has plenty of his own. END",
+    options: ["Restart"]
+  },
+  {
+    choice: "Pass",
+    prompt: `4b Albie tells you that he is a frequent winner of the gold hunt, and has plenty of gold already. He fills up a small pouch with a handful of gold coins and hands it to you. "Here, you need it more than I do." END`,
+    options: ["Restart"]
+  },
 ];
 
 const prompt = document.querySelector(".prompt");
